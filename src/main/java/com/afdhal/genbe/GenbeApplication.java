8a497b783b8957955455908d0239eec9f973293d
yusara <yusraafdhal@gmail.com>
2020-08-28 06:05:50 +0700

    init commit

---
+++ src/main/java/com/afdhal/genbe/GenbeApplication.java
@@ -0,0 +1,13 @@
+package com.afdhal.genbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afdhal/genbe/GenbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afdhal.genbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
